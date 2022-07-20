a1a909686c328ec61e60e5c2957f1e76f99fe48a
Neeraj Kumar <neeraj.kumar@scania.com>
2022-07-20 21:27:03 +0200

    Added DTO Projections

--- bookmarker-api/src/main/java/com/neeraj/bookmarkerapi/domain/BookmarkRepository.java
+++ bookmarker-api/src/main/java/com/neeraj/bookmarkerapi/domain/BookmarkRepository.java
@@ -1,6 +1,13 @@
 package com.neeraj.bookmarkerapi.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark,Long> {
+
+    @Query("select new com.neeraj.bookmarkerapi.domain.BookmarkDTO(b.id,b.title,b.url,b.createdAt) from Bookmark b")
+    Page<BookmarkDTO> findBookmarks(Pageable pageable);
+
 }
--- bookmarker-api/src/main/java/com/neeraj/bookmarkerapi/domain/BookmarkService.java
+++ bookmarker-api/src/main/java/com/neeraj/bookmarkerapi/domain/BookmarkService.java
@@ -1,6 +1,7 @@
 package com.neeraj.bookmarkerapi.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -15,11 +16,13 @@ import java.util.List;
 public class BookmarkService {
 
     private final BookmarkRepository repository;
+    private final BookmarkMapper bookmarkMapper;
 
     @Transactional(readOnly=true)
     public BookmarksDTO getBookmarks(Integer page){
         int pageNo = page <1 ? 0 : page-1 ;
         Pageable pageable = PageRequest.of(pageNo,10, Sort.Direction.DESC,"createdAt");
-        return new BookmarksDTO(repository.findAll(pageable));
+        Page<BookmarkDTO> bookmarkPage= repository.findBookmarks(pageable);
+        return new BookmarksDTO(bookmarkPage);
     }
 }
